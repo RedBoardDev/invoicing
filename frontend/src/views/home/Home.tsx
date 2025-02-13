e1679865515cbb82e7c045e9452a5c9415601973
RedBoardDev <thomas.ott@epitech.eu>
2025-02-13 22:39:48 +0200

    test

--- frontend/src/views/home/Home.tsx
+++ frontend/src/views/home/Home.tsx
@@ -4,7 +4,7 @@ import styles from './Home.module.css';
 const Home: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to Your Frontend Template</h1>
+      <h1 className={styles.title}>dWelcome to Your Frontend Template</h1>
       <p className={styles.subtitle}>A scalable and modular starter for building modern React applications.</p>
     </div>
   );
