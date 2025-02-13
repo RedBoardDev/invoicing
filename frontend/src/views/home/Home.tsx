0cff85cf9bc38373b52e67ac8666c84d5b49efa9
RedBoardDev <thomas.ott@epitech.eu>
2025-02-14 00:06:52 +0200

    d

--- frontend/src/views/home/Home.tsx
+++ frontend/src/views/home/Home.tsx
@@ -1,13 +1,15 @@
-import type React from 'react';
-import styles from './Home.module.css';
+import type React from "react";
+import styles from "./Home.module.css";
 
 const Home: React.FC = () => {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to Your Frontend Template</h1>
-      <p className={styles.subtitle}>A scalable and modular starter for building modern React applications.</p>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>Welcome to Your Frontend Template</h1>
+			<p className={styles.subtitle}>
+				A scalable and modular starter for building modern React applications.
+			</p>
+		</div>
+	);
 };
 
 export default Home;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,33 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import path from 'node:path';
+import { defineConfig, mergeConfig } from 'vite';
+import baseConfig from './config/vite/base.config';
 
-export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    port: 3002,
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '@icon': path.resolve(__dirname, 'src/components/Icon.tsx'),
-      '@icons': path.resolve(__dirname, 'src/assets/icons'),
-      '@images': path.resolve(__dirname, 'src/assets/images'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@enums': path.resolve(__dirname, 'src/enums'),
-      '@api': path.resolve(__dirname, 'src/services/api'),
-      '@services': path.resolve(__dirname, 'src/services'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@utils': path.resolve(__dirname, 'src/utils/index.ts'),
-      '@interfaces': path.resolve(__dirname, 'src/interfaces'),
-      '@views': path.resolve(__dirname, 'src/views'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@config': path.resolve(__dirname, 'src/config'),
-      '@routes': path.resolve(__dirname, 'src/routes'),
+export default defineConfig(({ mode }) => {
+  return mergeConfig(baseConfig, {
+    server: {
+      port: 3002,
+      open: true,
     },
-  },
-  build: {
-    outDir: 'build',
-  },
+  });
 });
