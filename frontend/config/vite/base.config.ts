412a9a4260cc912fd31df23dcdaaa856157f6d9f
RedBoardDev <thomas.ott@epitech.eu>
2025-02-14 14:01:39 +0200

    d

--- frontend/config/vite/base.config.ts
+++ frontend/config/vite/base.config.ts
@@ -3,46 +3,59 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-	plugins: [
-		react(),
-		svgr({
-			svgrOptions: {
-				icon: true,
-				svgo: true,
+export default (mode: string) =>
+	defineConfig({
+		plugins: [
+			react(),
+			svgr({
+				svgrOptions: {
+					icon: true,
+					svgo: true,
+				},
+			}),
+		],
+		resolve: {
+			alias: {
+				"@icon": path.resolve(__dirname, "src/components/Icon.tsx"),
+				"@icons": path.resolve(__dirname, "src/assets/icons"),
+				"@images": path.resolve(__dirname, "src/assets/images"),
+				"@components": path.resolve(__dirname, "src/components"),
+				"@enums": path.resolve(__dirname, "src/enums"),
+				"@api": path.resolve(__dirname, "src/services/api"),
+				"@services": path.resolve(__dirname, "src/services"),
+				"@contexts": path.resolve(__dirname, "src/contexts"),
+				"@utils": path.resolve(__dirname, "src/utils/index.ts"),
+				"@interfaces": path.resolve(__dirname, "src/interfaces"),
+				"@views": path.resolve(__dirname, "src/views"),
+				"@hooks": path.resolve(__dirname, "src/hooks"),
+				"@config": path.resolve(__dirname, "src/config"),
+				"@routes": path.resolve(__dirname, "src/routes"),
 			},
-		}),
-	],
-	resolve: {
-		alias: {
-			"@icon": path.resolve(__dirname, "src/components/Icon.tsx"),
-			"@icons": path.resolve(__dirname, "src/assets/icons"),
-			"@images": path.resolve(__dirname, "src/assets/images"),
-			"@components": path.resolve(__dirname, "src/components"),
-			"@enums": path.resolve(__dirname, "src/enums"),
-			"@api": path.resolve(__dirname, "src/services/api"),
-			"@services": path.resolve(__dirname, "src/services"),
-			"@contexts": path.resolve(__dirname, "src/contexts"),
-			"@utils": path.resolve(__dirname, "src/utils/index.ts"),
-			"@interfaces": path.resolve(__dirname, "src/interfaces"),
-			"@views": path.resolve(__dirname, "src/views"),
-			"@hooks": path.resolve(__dirname, "src/hooks"),
-			"@config": path.resolve(__dirname, "src/config"),
-			"@routes": path.resolve(__dirname, "src/routes"),
 		},
-	},
-	css: {
-		modules: {
-			localsConvention: "camelCaseOnly",
+		css: {
+			modules: {
+				localsConvention: "camelCaseOnly",
+			},
 		},
-	},
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					vendor: ["react", "react-dom", "react-router-dom"],
+		build: {
+			outDir: "dist",
+			sourcemap: mode === "development" ? "inline" : false,
+			minify: mode === "production" ? "terser" : "esbuild",
+			terserOptions: {
+				compress: {
+					drop_console: mode === "production",
+					drop_debugger: true,
+				},
+				format: {
+					comments: false,
+				},
+			},
+			rollupOptions: {
+				output: {
+					manualChunks: {
+						vendor: ["react", "react-dom", "react-router-dom"],
+					},
 				},
 			},
 		},
-	},
-});
+	});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig, mergeConfig } from "vite";
 import baseConfig from "./config/vite/base.config";
 
 export default defineConfig(({ mode }) => {
-	return mergeConfig(baseConfig, {
+	return mergeConfig(baseConfig(mode), {
 		server: {
 			port: 3002,
 			open: true,
+			strictPort: true,
 		},
 	});
 });
