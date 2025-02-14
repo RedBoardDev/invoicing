67b162a629de1b25c9105051de3307c35038cda4
RedBoardDev <thomas.ott@epitech.eu>
2025-02-14 15:33:51 +0200

    d

--- frontend/config/vite/base.config.ts
+++ frontend/config/vite/base.config.ts
@@ -1,4 +1,3 @@
-import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
@@ -14,24 +13,6 @@ export default (mode: string) =>
 				},
 			}),
 		],
-		resolve: {
-			alias: {
-				"@icon": path.resolve(__dirname, "src/components/Icon.tsx"),
-				"@icons": path.resolve(__dirname, "src/assets/icons"),
-				"@images": path.resolve(__dirname, "src/assets/images"),
-				"@components": path.resolve(__dirname, "src/components"),
-				"@enums": path.resolve(__dirname, "src/enums"),
-				"@api": path.resolve(__dirname, "src/services/api"),
-				"@services": path.resolve(__dirname, "src/services"),
-				"@contexts": path.resolve(__dirname, "src/contexts"),
-				"@utils": path.resolve(__dirname, "src/utils/index.ts"),
-				"@interfaces": path.resolve(__dirname, "src/interfaces"),
-				"@views": path.resolve(__dirname, "src/views"),
-				"@hooks": path.resolve(__dirname, "src/hooks"),
-				"@config": path.resolve(__dirname, "src/config"),
-				"@routes": path.resolve(__dirname, "src/routes"),
-			},
-		},
 		css: {
 			modules: {
 				localsConvention: "camelCaseOnly",
@@ -40,16 +21,6 @@ export default (mode: string) =>
 		build: {
 			outDir: "dist",
 			sourcemap: mode === "development" ? "inline" : false,
-			minify: mode === "production" ? "terser" : "esbuild",
-			terserOptions: {
-				compress: {
-					drop_console: mode === "production",
-					drop_debugger: true,
-				},
-				format: {
-					comments: false,
-				},
-			},
 			rollupOptions: {
 				output: {
 					manualChunks: {
