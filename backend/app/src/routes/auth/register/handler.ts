ed61bc90d5305eb4888f1d7e5d51024f4eadcb65
RedBoardDev <thomas.ott@epitech.eu>
2025-02-15 17:34:14 +0200

    fix(backend): check if user already exist on register

--- backend/app/src/routes/auth/register/handler.ts
+++ backend/app/src/routes/auth/register/handler.ts
@@ -1,10 +1,17 @@
+import { ErrorsEnum } from '@enums/errors-enums';
 import { HttpStatusCode } from '@enums/http-status-enums';
-import { createUser } from '@repositories/user-repository';
+import ApiError from '@libs/error-management/api-error';
+import { createUser, getUserByEmail } from '@repositories/user-repository';
 import type { FastifyReply, FastifyRequest } from 'fastify';
 import type { TBody } from './schemas';
 
 const registerHandler = async (req: FastifyRequest, res: FastifyReply): Promise<void> => {
   const { email, password } = req.body as TBody;
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    throw new ApiError('User already exists', HttpStatusCode.conflict, ErrorsEnum.ressourceAlreadyExists);
+  }
 
   const user = await createUser({ email, password });
 
