7086ae6cb3797c2ef069d8cf0d73d867857a4356
RedBoardDev <thomas.ott@epitech.eu>
2025-02-28 18:17:54 +0200

    refactor(backend): adapt pagination logic with extends

--- backend/app/src/entities/pagination-query.ts
+++ backend/app/src/entities/pagination-query.ts
@@ -3,10 +3,10 @@ import type { FromSchema } from 'json-schema-to-ts';
 export const paginationQuerySchema = {
   type: 'object',
   properties: {
-    page: { type: 'string', pattern: '^[0-9]+$' },
-    pageSize: { type: 'string', pattern: '^[0-9]+$' },
-    findId: { type: 'string', pattern: '^[0-9]+$' },
-    includeCount: { type: 'string', enum: ['true', 'false'] },
+    page: { type: 'string', pattern: '^[0-9]+$', default: '1' },
+    pageSize: { type: 'string', pattern: '^[0-9]+$', default: '1000' },
+    findId: { type: 'string', format: 'uuid' },
+    totalCount: { type: 'string', default: false },
   },
   required: [],
   additionalProperties: false,
--- backend/app/src/libs/pagination.ts
+++ backend/app/src/libs/pagination.ts
@@ -1,18 +1,16 @@
+import type { PaginationQueryString } from '@entities/pagination-query';
 import { ErrorsEnum } from '@enums/errors-enums';
-import type { PaginationQueryString } from 'entities/pagination-query';
+import ApiError from '@libs/error-management/api-error';
 import type { FastifyRequest } from 'fastify';
-
-import ApiError from './error-management/api-error';
 
 export interface Pagination {
   page: number;
   pageSize: number;
-  elementIdxInPage?: number;
   findId?: string;
 }
 
 export interface PaginationParams {
-  pagination: Pagination;
+  pagination?: Pagination;
   includeTotalCount: boolean;
 }
 
@@ -21,16 +19,16 @@ export interface PaginationQuery {
   take: number;
 }
 
-export const paginationForQuery = (pagination: Pagination): PaginationQuery => ({
-  skip: (pagination.page - 1) * pagination.pageSize,
-  take: pagination.pageSize,
-});
-
 export interface CursorPaginationQuery {
   cursor?: { id: string };
   skip?: number;
   take: number;
 }
+
+export const paginationForQuery = (pagination: Pagination): PaginationQuery => ({
+  skip: (pagination.page - 1) * pagination.pageSize,
+  take: pagination.pageSize,
+});
 
 export const cursorPaginationForQuery = (pagination: Pagination): CursorPaginationQuery => {
   if (pagination.findId) {
@@ -52,14 +50,10 @@ export const paginationForComplexQuery = async (
 ): Promise<PaginationQuery | undefined> => {
   if (!pagination) return undefined;
   if (!pagination.findId) {
-    return {
-      skip: (pagination.page - 1) * pagination.pageSize,
-      take: pagination.pageSize,
-    };
+    return paginationForQuery(pagination);
   }
 
   const position = await countAboveFn();
-
   if (position === undefined) {
     throw new ApiError('Item not found', 404, ErrorsEnum.ressourceNotFound);
   }
@@ -67,24 +61,19 @@ export const paginationForComplexQuery = async (
   const pageNumber = Math.floor(position / pagination.pageSize) + 1;
   const skip = (pageNumber - 1) * pagination.pageSize;
 
-  pagination.elementIdxInPage = position % pagination.pageSize;
   pagination.page = pageNumber;
-
-  return {
-    skip,
-    take: pagination.pageSize,
-  };
+  return { skip, take: pagination.pageSize };
 };
 
 export const parsePaginationParams = (req: FastifyRequest): PaginationParams => {
   const query = req.query as PaginationQueryString;
 
-  const includeTotalCount = query.includeCount === 'true';
-  const page = Number.parseInt(query.page || '1', 10);
-  const pageSize = Number.parseInt(query.pageSize || '1000', 10); // on va mettre 1000 dans le cadre du projet, et éviter le besoin d'utiliser la pagination
-  const findId = query.findId ? query.findId : undefined;
+  const page = Number.parseInt(query.page ?? '1', 10);
+  const pageSize = Number.parseInt(query.pageSize ?? '100', 10);
+  const findId = query.findId ?? undefined;
+  const includeTotalCount = query.totalCount === 'true';
 
-  if (pageSize < 1 || page < 1) {
+  if (page < 1 || pageSize < 1) {
     throw new ApiError('Invalid page or pageSize', 400, ErrorsEnum.invalidQueryParameter);
   }
 
